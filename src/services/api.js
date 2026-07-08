eebae630612da7b1fd90d01b3f0ff6ac386235c7
Abhay Sahu <abhaysahugit@gmail.com>
2026-07-08 17:21:44 +0530

    Changed: Base URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:7000/api";
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "https://api.teenants.site";
 // const API_BASE_URL =
 //   import.meta.env.VITE_API_BASE_URL || "https://teenants-app.onrender.com";
 
